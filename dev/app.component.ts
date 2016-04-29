924ee679c5ba5c17e8e961126edee25315b6a08f
Nikhil Sonavane <nsonavane@integconsulting.com>
2016-04-29 19:19:41 +0530

    urlindex=4 , simple one way bind

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -3,10 +3,31 @@ import {Component} from 'angular2/core';
 @Component({
     selector: 'my-app',
     template: `
-        <h1>Angular 2 Boilerplate</h1>
-        <p>Hello World!</p>
+        <h1>Angular 2 demo</h1>
+        <h2
+        (click)="showSelect()"
+        [class.clicked]=  "showDetail === true"
+        >
+        {{contact.firstname}}   {{contact.lastname}}<br>
+        
+        </h2>
+        <div *ngIf = "showDetail === true" >
+        Phone Number:{{contact.pnum}}<br/>
+        Email : {{contact.email}}
+        </div>
     `,
+    styleUrls:[".//src/css/app.css"];
 })
 export class AppComponent {
+public  contact = { firstname:"nikhil", lastname:"sonavane", pnum:"12345678" , email:"paytm@gmail.com"};
+
+public showDetail = false;
+public showSelect(){
+this.showDetail=true;
+}
+
+public showLog(){
+console.debug(this.contact.firstname);
+}
 
 }
