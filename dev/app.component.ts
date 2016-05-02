6e9055d405a23ea12553d41bec61dd03752ab828
Nikhil Sonavane <nsonavane@integconsulting.com>
2016-05-02 13:04:09 +0530

    NOT WORK

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -9,6 +9,8 @@ import {Component} from 'angular2/core';
         [class.clicked]=  "showDetail === true"
         >
         {{contact.firstname}}   {{contact.lastname}}<br>
+        <button (click)="privRecord()" > &lt; </button>
+        <button (click)="nextRecord()" > &gt; </button><br>
         <input type="text" [(ngModel)]="contact.firstname" (click)="showLog()"   ><br>
         </h2>
         <div *ngIf = "showDetail === true" >
@@ -19,7 +21,37 @@ import {Component} from 'angular2/core';
     styleUrls:[".//src/css/app.css"];
 })
 export class AppComponent {
-public  contact = { firstname:"nikhil", lastname:"sonavane", pnum:"12345678" , email:"paytm@gmail.com"};
+
+public  contactList=[];
+ public contact={};
+ public curtrentRec  ;
+
+constructor() {
+  this.contactList= [
+          { firstname:"nikhil", lastname:"sonavane", pnum:"12345678" , email:"paytm@gmail.com"},
+          { firstname:"nikhil2", lastname:"sonavane2", pnum:"123456782" , email:"paytm@gmail.com2"},
+          { firstname:"nikhil3", lastname:"sonavane3", pnum:"123456783" , email:"paytm@gmail.com3"}
+    ];
+    this.curtrentRec = 1;
+    console.log(this.curtrentRec);
+    this.contact= this.contactList[0];
+
+}
+
+publi privRecord(){
+
+this.curtrentRec =  this.curtrentRec  -1
+console.error(this.curtrentRec );
+this.curtrentRec =  1;
+console.error(this.curtrentRec );
+this.contact= this.contactList[ 1 ];
+}
+
+public nextRecord(){
+this.curtrentRec=this.curtrentRec + 1;
+this.contact= this.contactList[ 2 ];
+}
+
 
 public showDetail = false;
 public showSelect(){
